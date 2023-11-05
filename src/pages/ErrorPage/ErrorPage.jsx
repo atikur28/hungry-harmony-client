9a0ac4eea89160f62d17d1c318e9b3fa655a633f
atikur28 <atikur45683@gmail.com>
2023-11-05 12:14:22 +0600

    Error page Added

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,9 +3,9 @@ import { useRouteError } from "react-router-dom";
 const ErrorPage = () => {
   const error = useRouteError();
   return (
-    <div className="text-center my-10 md:my-40 lg:my-96 space-y-4">
+    <div className="text-center my-10 md:my-20 space-y-4">
       <h1 className="text-4xl font-semibold">Oops!!!..</h1>
-      <img src="https://i.ibb.co/m979Y2T/7967799-3819627.jpg" alt="" />
+      <img className="md:w-[500px] mx-auto" src="https://i.ibb.co/m979Y2T/7967799-3819627.jpg" alt="" />
       <p className="font-semibold">
         <i>404 {error.statusText || error.message}!!</i>
       </p>
